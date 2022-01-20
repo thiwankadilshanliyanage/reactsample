6b722b4fac9eb39913734b0406ae629912f8fd55
thiwankadilshanliyanage <79803162+thiwankadilshanliyanage@users.noreply.github.com>
2022-01-20 22:57:23 +0530

    first commit

---
+++ src/App2.js
@@ -0,0 +1,25 @@
+ import './App.css';
+import React ,{ Component }  from "react";
+const app1=()=>{
+    return(
+      <div className='app2'>
+        <h2>Hey</h2>
+      </div>
+    );
+  }
+   
+  export default app1;
+
+
+ 
+
+// class Post extends Component{
+//     render(){
+//         return(
+//             <div className="Post">
+//                 <h1>Heyyyyyy</h1>
+//             </div>
+
+//         )
+//     }
+// }
